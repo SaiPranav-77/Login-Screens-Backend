c364ae039614427f28c370d3741ac9b2c1494cc1
Sai Pranav <saipranav071@gmail.com>
2026-03-04 21:48:36 +0530

    fix mongo connection

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-// import express from 'express';
-var express = require('express')
-var cors = require('cors')
-var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
-var connect=require('mongoose').connect
-var authRoutes = require('./routes/auth.js')
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.js");
 
+const app = express();
 
-const app = express()
-app.use(cors()) 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {
-
-}).then(() =>console.log("MongoDB connected"))
-.catch(err => console.log("MongoDb connection error:", err))
-
-app.use('/', authRoutes)
-app.listen(5005, () => {
-  console.log('Server is running on http://localhost:5005')
+// MongoDB connection (use environment variable)
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB connected");
 })
+.catch(err => {
+  console.log("MongoDB connection error:", err);
+});
 
 mongoose.connection.once("open", () => {
   console.log("Connected DB:", mongoose.connection.name);
+});
+
+// routes
+app.use("/", authRoutes);
+
+// Render requires dynamic port
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
